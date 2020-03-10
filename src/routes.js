65ab19b63a3cb021c304defa4bc1d3e4b8f074cf
Todd Li <todd@tinybeans.com>
2020-03-10 12:59:57 +1100

    add loading spinner

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import App from "./App";
 import Cutup from "./views/Cutup";
+import CutupLoading from "./views/CutupLoading";
 
 const AllRoutes = () => (
   <Router>
     <Switch>
       <Route path="/" component={App} exact />
       <Route path="/cutup" component={Cutup} />
+      <Route path="/loading" component={CutupLoading} />
     </Switch>
   </Router>
 );
---
+++ src/views/CutupLoading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const CutupLoading = () => (
+  <div className="loader-container isLoading">
+    <div className="loader">Loading...</div>
+  </div>
+);
+export default CutupLoading;
