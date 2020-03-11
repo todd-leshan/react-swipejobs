2fd030b19a52230db5355b1a7a32d8634272a3ba
Todd Li <todd@tinybeans.com>
2020-03-11 23:09:07 +1100

    Updates

---
+++ precache-manifest.3bbbd5d61f021c10d189f290dd916953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394d7062c21a0cf1696abd0d78135e9f",
+    "url": "/react-swipejobs/index.html"
+  },
+  {
+    "revision": "0484b264a9053d790de7",
+    "url": "/react-swipejobs/static/css/main.10b8de96.chunk.css"
+  },
+  {
+    "revision": "376a3de4eb5aff9abed0",
+    "url": "/react-swipejobs/static/js/2.b1ec1542.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-swipejobs/static/js/2.b1ec1542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0484b264a9053d790de7",
+    "url": "/react-swipejobs/static/js/main.6fa42ce9.chunk.js"
+  },
+  {
+    "revision": "c77e6b49e8fbbd0cd7b8",
+    "url": "/react-swipejobs/static/js/runtime-main.7981197b.js"
+  }
+]);
