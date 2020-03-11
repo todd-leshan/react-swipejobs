6cee5ab909ec48e6efbb81b1e54bf7e8ec49e001
Todd Li <todd@tinybeans.com>
2020-03-11 14:49:16 +1100

    error message style

--- src/components/global/Error.jsx
+++ src/components/global/Error.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Error = props => <p>{props.errorMsg}</p>;
+const Error = props => <p className="msg--error">{props.errorMsg}</p>;
 
 Error.propTypes = {
   errorMsg: PropTypes.string.isRequired
