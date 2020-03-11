bfaf2976edeaa62fb27afe6783467b0242485ac1
Todd Li <todd@tinybeans.com>
2020-03-11 23:04:19 +1100

    use HashRouter and rebuild docs folder

---
+++ docs/precache-manifest.a336e05d7c261c82b37415eae9e29f85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee0c7f6cd7e8d3ac677d9ba4898ce1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "604666e1052aff23b26f",
+    "url": "/static/css/main.10b8de96.chunk.css"
+  },
+  {
+    "revision": "376a3de4eb5aff9abed0",
+    "url": "/static/js/2.b1ec1542.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.b1ec1542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604666e1052aff23b26f",
+    "url": "/static/js/main.13c5b511.chunk.js"
+  },
+  {
+    "revision": "7793e50223aa0704e6b5",
+    "url": "/static/js/runtime-main.98550e2c.js"
+  }
+]);
